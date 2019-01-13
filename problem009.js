997c811106b8bcf3711aaa020c50a2d31c7af5a2
Bernd Naehler <bernd@rockaBe.de>
2019-01-13 18:06:47 +0100

    P001: add solution to question :D

--- problem009.js
+++ problem009.js
@@ -20,3 +20,4 @@ for (a=1; a<s/3; a++) {
 
 console.log(`${a}^2 + ${b}^2 = ${c}^2 => ${a*a} + ${b*b} = ${c*c}`);
 console.log(`-- keeping the sum of all 3 numbers ${a} + ${b} + ${c} = 1000`);
+console.log(`the product a*b*c equals: ${a*b*c}`);
